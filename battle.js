f5e78c4a5d2089fa90b68dd51992a662713d25ae
Renato2707 <52422699+Renato2707@users.noreply.github.com>
2026-08-26 00:26:53 -0300

    fix(engine): restore battle.js (applyTeamTroops + full combat loop)

--- battle.js
+++ battle.js
@@ -1,1 +1,8 @@
-// battle.js restored - see next
+// battle.js
+
+import {
+  calculateFinalDamage, sortByInitiative, isTeamAlive, rollChance,
+  formatStatName, formatDamageTypeName, formatStatusName, formatDuration,
+  formatSignedPercent, formatTroopCapacity, isGrantedStatus, formatStackName,
+  applyChanceIf
+} from './utils.js';
