de0b83883e039d747293a55c4c69ac9fa3daf5d1
Renato2707 <52422699+Renato2707@users.noreply.github.com>
2026-08-28 22:42:56 -0300

    test: Kalspire dummy 10-round kit review (BA target, Bleed/Panic, Radiant Stun)

---
+++ test-kalspire.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import { Character } from './character.js';
+import { Battle } from './battle.js';
+import { applyInitiativeOrder } from './hook-initiative-order.js';
+import { applyVanguardLabel } from './hook-vanguard-label.js';
+import { applyLinkedRetreated } from './hook-linked-retreated.js';
+import { applyRetreatedPerTarget } from './hook-retreated-per-target.js';
+import { applyAfterBasicTarget } from './hook-after-basic-target.js';
+import { loadDragonHabitsSync, loadCommandSync } from './habitParser.js';
+import { VANGUARD_NAMES } from './vanguardNames.js';
+import { formatBattleReport } from './reportFormat.js';
+import { hasEffect, getEffect } from './effects.js';
+import { getDealerType } from './positionSystem.js';
+
+applyInitiativeOrder(Battle);
+applyVanguardLabel(Battle);
+applyLinkedRetreated(Battle);
+applyRetreatedPerTarget(Battle);
+applyAfterBasicTarget(Battle);
