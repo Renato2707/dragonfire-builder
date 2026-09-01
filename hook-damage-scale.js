18db9d1e58e5ecbd40589e9e8a43c9f0d35dd7ab
Renato2707 <52422699+Renato2707@users.noreply.github.com>
2026-09-01 18:14:03 -0300

    general print math: instinct heal + uniform damage coefficient

--- hook-damage-scale.js
+++ hook-damage-scale.js
@@ -1,21 +1,3 @@
-import { calculateTroopCapacity } from './troopCapacity.js';
-
-const TROOP_DAMAGE_REF = 2400;
-const TROOP_SCALE_FLOOR = 6500;
-
-export function applyDamageTroopScale(Battle) {
-  if (Battle.prototype.__damageTroopScale) return;
-  Battle.prototype.__damageTroopScale = true;
-  const orig = Battle.prototype.dealDamage;
-  Battle.prototype.dealDamage = function (target, amount, info) {
-    const source = info && info.source;
-    const formulaCap = source
-      ? calculateTroopCapacity(source.level, source.stars)
-      : 0;
-    let scaled = amount;
-    if (source && formulaCap >= TROOP_SCALE_FLOOR && amount > 0) {
-      scaled = Math.max(1, Math.round(amount * (formulaCap / TROOP_DAMAGE_REF)));
-    }
-    return orig.call(this, target, scaled, info);
-  };
-}
+// Damage intensity lives in utils.calculateFinalDamage (PRINT_DAMAGE_SCALE).
+// Kept so old imports do not break.
+export function applyDamageTroopScale() {}
